f1be9e4ec76af83c071373550a8c48ea5f4c08fd
Will <wjdhollow@gmail.com>
2011-05-27 22:17:25 -0700

    Removed user_id field from the event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -68,7 +68,7 @@ class EventsController < ApplicationController
   # POST /events.xml
   def create
     @event = Event.new(params[:event])
-
+    @event.user_id = current_user.id
     respond_to do |format|
       if @event.save
         format.html { redirect_to(@event, :notice => 'Event was successfully created.') }
